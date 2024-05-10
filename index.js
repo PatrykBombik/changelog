0dac49742dec58ef0512435888e5eb7027bb3faf
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-10 22:06:51 +0200

    express basic

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.json({message: 'Hello Express!'});
+    // res.json({message: 'Hello Express!'});
+    res.sendFile(path.resolve("pages/index.html"))
 })
 
 app.listen(3002, () => {
