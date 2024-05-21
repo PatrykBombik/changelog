e90e567c950ffaf1b3cf1f103b771f722a35392a
Patryk Bombik <patryk.bombik@gmail.com>
2024-05-21 22:53:35 +0200

    signin done

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import router from './router'
+import {protect} from "./modules/auth";
+import {createNewUser, signIn} from "./handlers/user";
 
 const app = express();
 
@@ -8,10 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', router);
+app.use('/api/v1', protect, router);
 
-app.get('/', (req, res) => {
-    res.json({message: 'Hello Express!'});
-})
+app.post('/users', createNewUser);
+app.post('/signin',signIn)
 
 export default app;
