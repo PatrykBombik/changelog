c6ac3eb0ef71cfab47c9adbffa18048ac8d9b2a9
PatrykBombik <patryk.bombik@gmail.com>
2024-05-28 16:30:03 +0200

    routes update

--- src/router.ts
+++ src/router.ts
@@ -11,10 +11,10 @@ router.post('/products', () => {});
 router.put('/products/:id', () => {});
 router.delete('/products/:id', () => {});
 
-router.get('/products', () => {});
-router.get('/products/:id', () => {});
-router.post('/products', () => {});
-router.put('/products/:id', () => {});
-router.delete('/products/:id', () => {});
+router.get('/updates', () => {});
+router.get('/updates/:id', () => {});
+router.post('/updates', () => {});
+router.put('/updates/:id', () => {});
+router.delete('/updates/:id', () => {});
 
 export default router;
